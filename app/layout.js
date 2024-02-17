809d80917dfc638e56eb38f293b2bd0b815156ac
ADIL-SALAH <122431444+ADIL-SALAH@users.noreply.github.com>
2024-02-17 13:44:26 +0530

    build error solved

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { Suspense } from 'react'
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <Suspense>
+          <Navbar />
+          {children}
+        </Suspense>
+
       </body>
     </html>
   );
